17cd200ec0657fc592973a2937c84a40c24029be
Priyanshujain25 <priyanshuj.ug22.ee@nitp.ac.in>
2025-09-17 17:58:12 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -7,5 +7,7 @@ export const initSocket = async () => {
     timeout: 10000,
     transports: ["websocket"],
   };
-  return io(process.env.REACT_APP_BACKEND_URL, options);
+  return io({
+  transports: ["websocket", "polling"],
+});
 };
